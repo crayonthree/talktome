f4561fff03b139ff264377bf61e963f1ec1a5902
Abhiroop <lactosetheintolerant2@yahoo.com>
2026-07-02 23:02:27 -0400

    feature: added basic conversational responses (hardcoded)

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # talktome
 Smart text-based chatbot
 
-## Project Overview (TO BE UPATED)
+## Project Overview (TO BE UPDATED)
 
 ## Latest Version
 ### v1 (In Development)
 
 ## Version Timeline
-### v1 (2026) (TO BE UPATED)
+### v1 (2026) (TO BE UPDATED)
     -   ML based text-chatbot.
 
-## Long Term Vision (TO BE UPATED)
+## Long Term Vision (TO BE UPDATED)
 
-## Tech Stack (TO BE UPATED)
+## Tech Stack (TO BE UPDATED)
 
--- v1/model/main.py
+++ v1/model/main.py
@@ -7,8 +7,16 @@ print(f"Hello, {name}!")
 while True:
     user_input = input("Abhi: ")
     
-    if user_input.lower() == 'exit':
+    if user_input.lower() == 'bye' or user_input.lower() == 'goodbye':
         print(f"Goodbye, {name}!")
         break
-    
-    print(f"You said: {user_input}")
+    elif user_input.lower() == "what is your name":
+        print("My name is talktome, and I love lowercase letters.")
+    elif user_input.lower() == 'hello' or user_input.lower() == 'hi':
+        print(f"Hello, {name}! How can I assist you today?")
+    elif user_input.lower() == 'how are you':
+        print("I am doing well, even though I don't have feelings :)")
+    elif user_input.lower() == 'thanks' or user_input.lower() == 'thank you' or user_input.lower() == 'thankyou':
+        print("You are welcome! What else can I assist you with today?")
+    else:
+        print(f"You said {user_input}, I am not sure I understand you.")
