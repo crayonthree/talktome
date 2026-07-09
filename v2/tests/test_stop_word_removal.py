8e55e098216bd2ad9fc433e4e0583bedc57c6fe0
Abhiroop <lactosetheintolerant2@yahoo.com>
2026-07-08 22:17:04 -0400

    chore: added requirements.txt for dependencies, intialized stop word removal testing.

---
+++ v2/tests/test_stop_word_removal.py
@@ -0,0 +1,12 @@
+from model import nlp_processor
+
+## TO DO CASES (Inputs) ##
+
+# Some specific stop words.
+# Only stop words
+# No stop words
+# Empty String
+# Empty Words in the String
+# Empty words with Stop words and non stop words.
+
+# *Remember to pass a list and not a string*
